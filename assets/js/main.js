cb87c6e011e6dc9cb6a8422cd7902badf3bfbd3f
Firuddin <Firuddinaliyev888@gmail.com>
2024-07-17 01:04:19 +0400

    add navbar mobile show menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
 /*=============== SHOW MENU ===============*/
+const navMenu=document.getElementById('nav-menu')
+navToggle =document.getElementById('nav-toggle')
+navClose = document.getElementById('nav-close')
 
 
+/*Menu goster*/
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*Menu gizlet*/
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 /*=============== REMOVE MENU MOBILE ===============*/
 
 
