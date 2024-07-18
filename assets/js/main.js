a79e5ead372beabc315942a2ed9d95f717f1d756
Firuddin <Firuddinaliyev888@gmail.com>
2024-07-19 03:03:56 +0400

    add Remove menu Mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,13 @@ if(navClose){
     })
 }
 /*=============== REMOVE MENU MOBILE ===============*/
+const navlink = document.querySelectorAll('.nav__link')
 
+const linkAction = ()=>{
+    const navMenu =document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navlink.forEach(n=>n.addEventListener('click',linkAction))
 
 /*=============== ADD SHADOW HEADER ===============*/
 
