2d97f78c1d1abd70a3e7a1e744cb5bee1fd207fb
Firuddin <firuddinaliyev888@gmail.com>
2024-07-28 16:54:29 +0400

    add scrolUpp the end

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,12 @@ const shadowHeader = () =>{
 window.addEventListener('scroll',shadowHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp=()=>{
+    const scrollUp =document.getElementById('scroll-up')
+    this.scrollY>=350? scrollUp.classList.add('show-scroll')
+                     : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
