a1470da4c0f75d62db4740a66720b489f8d7e635
Firuddin <firuddinaliyev888@gmail.com>
2024-08-03 11:29:47 +0400

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,6 +43,26 @@ const scrollUp=()=>{
 window.addEventListener('scroll',scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections =document.querySelectorAll('section[id]')
 
+
+const scrollActive = ()=>{
+  const scrollDown =window.scrollY
+
+
+  sections.forEach(current=>{
+       const sectionHeight = current.offsetHeight,
+             sectionTop =current.offsetTop-58,
+             sectinId =current.getAttribute('id'),
+             sectionsClass =document.querySelector('.nav__menu a[href*='+ sectinId +']')
+
+        if (scrollDown> sectionTop && scrollDown<= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+  })
+}
+window.addEventListener('scroll',scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
